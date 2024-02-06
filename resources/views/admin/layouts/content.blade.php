8bc4553f5d85c5dc54cdb25418a2cafaa5c388fe
ramananda-at-251313237507 <rshc4u@gmail.com>
2024-02-06 15:40:50 +0600

    udpated dashboard content

--- resources/views/admin/layouts/content.blade.php
+++ resources/views/admin/layouts/content.blade.php
@@ -8,42 +8,54 @@
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Primary Card</div>
+                                    <div class="card-body">Users
+                                        <p><i class="fas fa-user fa-fw" style="font-size:20px;"></i></p>
+                                    </div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                        <a class="small text-white stretched-link" style="font-size:18;" href="#">{{App\Models\User::all()->count()}}</a>
                                     </div>
                                 </div>
                             </div>
+
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-warning text-white mb-4">
-                                    <div class="card-body">Warning Card</div>
+                                    <div class="card-body">Department
+                                        <p><i class="fas fa-home" style="font-size:18;"></i></p>
+                                    </div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                        <a class="small text-white stretched-link" href="#">
+                                            {{App\Models\Department::all()->count()}}
+                                        </a>
+
                                     </div>
                                 </div>
                             </div>
+
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Success Card</div>
+                                    <div class="card-body">Notices
+                                        <p><i class="fas fa-book" style="font-size:20px;"></i></p>
+                                    </div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                        <a class="small text-white stretched-link" href="#">
+                                            {{App\Models\Notice::all()->count()}}
+                                        </a>
+                                       
                                     </div>
                                 </div>
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-danger text-white mb-4">
-                                    <div class="card-body">Danger Card</div>
+                                    <div class="card-body">Leave
+                                        <p><i class="fas fa-book" style="font-size:20px;"></i></p>
+                                    </div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                        <a class="small text-white stretched-link" href="#"> {{App\Models\Leave::all()->count()}}</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
+                        <!-- <div class="row">
                             <div class="col-xl-6">
                                 <div class="card mb-4">
                                     <div class="card-header">
@@ -62,7 +74,34 @@
                                     <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
                                 </div>
                             </div>
+                        </div> -->
+
+                        <div class="row">
+                            <div class="col-xl-12">
+                                <div class="card mb-4">
+                                    <div class="card-header">Yor details</div>
+                                    <div class="card-header" style="background-color:orange">
+                                        Name{{Auth::user()->name}}
+                                    </div>
+                                    <div class="card-header" style="background-color:orange">
+                                        Email: {{Auth::user()->email}}
+                                    </div>
+                                    <div class="card-header" style="background-color:orange">
+                                        Address: {{Auth::user()->address}}
+                                    </div>
+                                    <div class="card-header" style="background-color:orange">
+                                        Mobile: {{Auth::user()->mobile_number}}
+                                    </div>
+                                    <div class="card-header" style="background-color:orange">
+                                        Designation: {{Auth::user()->designation}}
+                                    </div>
+                                    <div class="card-header" style="background-color:orange">
+                                        Join Date: {{Auth::user()->start_from}}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table me-1"></i>
