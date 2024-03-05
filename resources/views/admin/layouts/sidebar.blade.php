8107dce67b258ce68ccb749d4bd49e267196d9ab
ramananda-at-251313237507 <rshc4u@gmail.com>
2024-03-05 15:46:09 +0600

    enable nabvar for employee table

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -10,10 +10,27 @@
                             </a>
                             <div class="sb-sidenav-menu-heading">Interface</div>
 
-                            <a class="nav-link" href="{{url('import-employee')}}">
+                            <!-- <a class="nav-link" href="{{url('import-employee')}}">
                                 <div class="sb-nav-link-icon"><i class="fas fa-user-alt"></i></div>
                                 Employee
+                            </a> -->
+
+
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayoutsEmp" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-user-alt"></i></div>
+                                Emplopyee
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
+
+                            <div class="collapse" id="collapseLayoutsEmp" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="{{Route('employee.index')}}">View</a>
+
+                                    <a class="nav-link" href="{{Route('employee.create')}}">Create</a>
+                                    
+                                </nav>
+                            </div>
+
 
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
