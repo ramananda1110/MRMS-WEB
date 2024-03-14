802b888bd050f71dff7951534dab22566a8512f6
ramananda-at-251313237507 <rshc4u@gmail.com>
2024-03-14 13:32:50 +0600

    reset password fixed

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\ResetsPasswords;
 
 use App\Models\User;
 use App\Http\Requests\ResetPasswordRequest;
-use Ichtrojan\Otp\Otp;
+use Otp;
 use Hash;
 
 class ResetPasswordController extends Controller
@@ -35,49 +35,23 @@ class ResetPasswordController extends Controller
 
     private $otp;
 
-    public function _costruct()
+    public function __construct(Otp $otp)
     {
-        $this->otp = new Otp;
+        $this->otp = $otp;
     }
 
-    // public function passwordReset(ResetPasswordRequest $request)
-    // {
-    //     $otp2 = $this->otp->validate($request->email, $request->otp);
-
-    //     if(!$otp2->status) {
-    //         return response()->json(['error' => $otp2], 401);
-    //     }
-    //     $user = User::where('email', $request->email)->first();
-    //     $user->update(['password' => Hash::make($request->password)]);
-    //     $user->tokens()->delete();
-    //     $sucess['success'] = true;
-    //     return response()->json($sucess, 200);
-    // }
-
     public function passwordReset(ResetPasswordRequest $request)
-        {
-            $otp2 = $this->otp;
+    {
+        $otp2 = $this->otp->validate($request->email, $request->otp);
 
-            if (!$otp2) {
-                return response()->json(['error' => 'OTP object not found'], 500);
-            }
-
-            $validationResult = $otp2->validate($request->email, $request->otp);
-
-            if (!$validationResult->status) {
-                return response()->json(['error' => $validationResult], 401);
-            }
-
-            $user = User::where('email', $request->email)->first();
-
-            if (!$user) {
-                return response()->json(['error' => 'User not found'], 404);
-            }
-
-            $user->update(['password' => Hash::make($request->password)]);
-            $user->tokens()->delete();
-
-            $success['success'] = true;
-            return response()->json($success, 200);
+        if(!$otp2->status) {
+            return response()->json(['error' => $otp2], 401);
         }
+        $user = User::where('email', $request->email)->first();
+        $user->update(['password' => Hash::make($request->password)]);
+        $user->tokens()->delete();
+        $success['success'] = true;
+        return response()->json($success, 200);
+    }
+
 }
