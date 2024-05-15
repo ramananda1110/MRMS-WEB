4a8ec2dc8eb8a5cc9b5491fb4f5cbd53904abfc1
Tahmid <tahmid.ratul6@gmail.com>
2024-05-15 14:39:38 +0600

    background added

--- resources/views/admin/layouts/fotter.blade.php
+++ resources/views/admin/layouts/fotter.blade.php
@@ -1,4 +1,4 @@
-<footer class="py-4 bg-light mt-auto">
+<footer class="py-4 bg-light">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
                             <div class="text-muted">Copyright &copy; DDC 2024</div>
@@ -20,45 +20,23 @@
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
         <script src="{{asset('template/dist/js/datatables-simple-demo.js')}}"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-        <script src=
-"https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js">
-    </script>
-    <script src=
-"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js">
-    </script>
- 
-    <!-- Include Moment.js CDN -->
-    <script type="text/javascript" src=
-"https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js">
-    </script>
- 
-    <!-- Include Bootstrap DateTimePicker CDN -->
- 
- 
-    <script src=
-"https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js">
-        </script>
+        
         
 
         <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+        
+
+
+        
         <script>
                 $( function() {
                     $( "#datepicker" ).datepicker({dateFormat:"yy-mm-dd"}).val();
                 } );
         </script>
+       
         <script>
                 $( function() {
-                    $( "#datetimefrom" ).datetimepicker({format:'hh:mm:ss a'}).val();
-                } );
-        </script>
-        <script>
-                $( function() {
-                    $( "#datetimeto" ).datetimepicker({format:'hh:mm:ss a'}).val();
-                } );
-        </script>
-        <script>
-                $( function() {
-                    $( "#datepicker1" ).datepicker({dateFormat:"yy-mm-dd"}).val();
+                    $( "#datepicker1" ).datetimepicker({format:"HH-mm"}).val();
                 } );
         </script>
 
