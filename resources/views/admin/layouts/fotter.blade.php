e14ad4d5eb9b696d77662728609bfab2a55160ec
ramananda1110 <cserama.tpi@gmail.com>
2024-05-19 11:12:52 +0600

    off past day selector for creating meeting

--- resources/views/admin/layouts/fotter.blade.php
+++ resources/views/admin/layouts/fotter.blade.php
@@ -30,8 +30,17 @@
         
         <script>
                 $( function() {
-                    $( "#datepicker" ).datepicker({dateFormat:"yy-mm-dd"}).val();
+                    $( "#datepicker2" ).datepicker({dateFormat:"yy-mm-dd"}).val();
                 } );
+        </script>
+
+        <script>
+                $(function() {
+                    $("#datepicker").datepicker({
+                        dateFormat: "yy-mm-dd",
+                        minDate: 0 // Disable past dates
+                    });
+                });
         </script>
        
         <script>
