6edb28d20b3fa55672ba3c1bf4a2497c6eccded6
Tahmid <tahmid.ratul6@gmail.com>
2024-06-02 12:40:05 +0600

    csv excel print button removed for room, department and notice

--- resources/views/admin/department/index.blade.php
+++ resources/views/admin/department/index.blade.php
@@ -14,35 +14,7 @@
                         <div class="ms-3">
                             <a href="{{ Route('departments.create') }}"><button type="button" class="btn btn-primary"><i class="fa fa-plus"></i> Add Department</button></a>
                         </div>
-                        <div class="btn-group d-flex justify-content-center align-items-center me-3">
-                            <div class="row gx-0">
-                                <div class="col-md">
-                                    <form action="#" method="post" target="_blank">
-                                        @csrf        
-                                        <button type="submit" class="btn btn-default buttons-csv border buttons-html5 btn-sm btn-block" tabindex="0" aria-controls="employees">
-                                            <span>csv</span>
-                                        </button>
-                                    </form>
-                                </div>
-                                <div class="col-md">
-                                    <form action="#" method="post" target="_blank">
-                                        @csrf        
-                                        <button class="btn btn-default buttons-csv border buttons-html5 btn-sm btn-block">Excel</button>
-                                    </form>
-                                </div>
-                                <div class="col-md">
-                                    <form action="#" method="get" target="_blank">
-                                        @csrf        
-                                        <button class="btn btn-default buttons-csv border buttons-html5 btn-sm btn-block">Pdf</button>
-                                    </form>
-                                </div>
-                                <div class="col-md">
-                                    <a class="btn btn-default buttons-print border buttons-html5 btn-sm btn-block" tabindex="0" aria-controls="employees">
-                                        <span>Print</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>  
+                   
                     </div>
                 </div>
             </div>
