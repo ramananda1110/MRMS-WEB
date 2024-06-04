44bcdac1c119879881ae536c8ad86d1250921651
Tahmid <tahmid.ratul6@gmail.com>
2024-06-04 11:45:22 +0600

    change password ui redesign done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,109 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-    <!-- Section: Design Block -->
-    <section class="text-center text-lg-start">
-        <style>
-            .cascading-right {
-                margin-right: -50px;
-            }
+    <section>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="container py-5 h-100 ">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                        <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                            <div class="card-body p-5 text-center shadow-lg">
 
-            @media (max-width: 991.98px) {
-                .cascading-right {
-                    margin-right: 0;
-                }
-            }
-        </style>
-
-        <!-- Jumbotron -->
-        <div class="container py-4">
-            <div class="row g-0 align-items-center">
-                <div class="col-lg-6 mb-5 mb-lg-0">
-                    <div class="card cascading-right bg-body-tertiary"
-                        style="
-            backdrop-filter: blur(30px);
-            ">
-                        <div class="card-body p-5 shadow-5 text-center">
-                            <h2 class="fw-bold mb-5">{{ __('Login') }}</h2>
-                            <form method="POST" action="{{ route('login') }}">
-                                @csrf
-                                <!-- 2 column grid layout with text inputs for the first and last names -->
-                                <div class="row">
-                                    <div class="row mb-3">
-                                        <label class="row-md-4 col-form-label text-md-start">{{ __('Employee ID') }}</label>
-
-                                        <div class="row-md-6">
-                                            <input type="number"
-                                                class="form-control @error('employee_id') is-invalid @enderror"
-                                                name="employee_id">
-
-                                            @error('employee_id')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
-
-                                    <div class="row mb-3">
-                                        <label for="password"
-                                            class="row-md-4 col-form-label text-md-start">{{ __('Password') }}</label>
-
-                                        <div class="row-md-6">
-                                            <input id="password" type="password"
-                                                class="form-control @error('password') is-invalid @enderror" name="password"
-                                                required autocomplete="current-password">
-
-                                            @error('password')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
+                                <img src="../images/mrms_logo_2.png" class="rounded  w-50 mb-4" alt="Cinque Terre">
 
 
-
-                                    <div class="row mb-3">
-                                        <div class="row-md-6 offset-md-4">
-                                            <a class="dropdown-item text-danger mb-2 ms-3" href="#!">Forgot Password</a>
-
-                                            {{-- <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                            <label class="form-check-label" for="remember">
-                                {{ __('Remember Me') }}
-                            </label>
-                        </div> --}}
-                                        </div>
-                                    </div>
-
-
-
-                                    <button type="submit"
-                                        class="ms-3 btn btn-primary btn-block mb-4 w-25 d-flex justify-content-center ">
-                                        {{ __('Login') }}
-                                    </button>
-
-                                    <!--
-                                @if (Route::has('password.request'))
-    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-    @endif -->
-
+                                <div data-mdb-input-init class="form-outline mb-4">
+                                    <h5 for="number" class="text-start">{{ __('Employee ID') }}</h5>
+                                    <input type="number"
+                                        id="employee_id"class="form-control  form-control-lg @error('employee_id') is-invalid @enderror"
+                                        name="employee_id" />
+                                    @error('employee_id')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                 </div>
-                            </form>
+
+                                <div data-mdb-input-init class="form-outline mb-4">
+                                    <h5 for="password" class="text-start">{{ __('Password') }}</h5>
+                                    <input type="password"
+                                        id="password"class="form-control  form-control-lg @error('employee_id') is-invalid @enderror"
+                                        name="password" />
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <!-- Checkbox -->
+                                <div class="form-check d-flex justify-content-end mb-4">
+                                    <a class="form-check-label" for="form1Example3"> Forgot Password? </a>
+                                </div>
+
+                                {{-- <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg btn-block"
+                                type="submit">Login</button> --}}
+
+                                <button type="submit"
+                                    class="btn btn-primary btn-block mb-4 w-50 d-flex justify-content-center mx-auto">
+                                    {{ __('Login') }}
+                                </button>
+
+                                <hr class="my-4">
+
+
+
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <div class="col-lg-6 mb-5 mb-lg-0">
-                    <img src="../images/dashboard.jpg" class="w-50 rounded-4 shadow-lg" alt="" />
-                </div>
             </div>
-        </div>
-        <!-- Jumbotron -->
+        </form>
     </section>
-    <!-- Section: Design Block -->
 @endsection
