aae9b9865d2cbef1b69b5c882b619e341c250bb2
Tahmid <tahmid.ratul6@gmail.com>
2024-06-13 10:37:37 +0600

    #46 navbar and sidebar color changed to light theme

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -40,9 +40,9 @@
 </head>
 
 <body class="sb-nav-fixed">
-    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+    <nav class="sb-topnav navbar navbar-expand navbar-light shadow-lg">
         <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3" href="{{ url('/') }}">{{ Auth()->user()->role->name }}</a>
+        <a class="navbar-brand ps-3 text-center" href="{{ url('/') }}">{{ Auth()->user()->role->name }}</a>
         <!-- Sidebar Toggle-->
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
                 class="fas fa-bars"></i></button>
